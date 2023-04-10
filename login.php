ad483837258bfd940448bda0210110457fc7209f
Jannatul Ferdous Lam <127182865+jannatsoftit@users.noreply.github.com>
2023-04-10 06:46:41 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,51 @@
-<!doctype html>
+ 
+ <?php
+ 
+session_start();
+
+$conn = new mysqli('localhost','root','','userauthentication');
+
+if(!$conn){
+  echo "Database Connection Failed!!";
+}
+
+$empty_email = $empty_password = "";
+ 
+if(isset($_POST['login'])){
+
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    $md5_user_password = md5($user_password);
+
+    if(empty($user_email)){
+        $empty_email = "Fill up this field";
+    }
+    if(empty($user_password)){
+        $empty_password = "Fill up this field";
+    }
+
+    if(!empty($user_email) && !empty($user_password)){
+        $sql = "SELECT * FROM users WHERE user_email = '$user_email' AND user_password = '$md5_user_password'";
+
+        $query = $conn->query($sql);
+        if($query->num_rows > 0){
+
+            $_SESSION['lognin'] = 'login successfull';
+
+            header('location:dashboard.php');
+        }else{
+            echo "Login Unsuccessfull";
+        }
+
+    }
+
+}
+
+
+?>
+
+ 
+ <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -18,16 +65,24 @@
             <div class="col-4"></div>
 
             <div class="col-4" style="margin-top:50px;">
+
+            <?php if(isset($_GET['usercreated'])){ 
+                echo "Information Added Successfully";
+            } ?>
                 <form action="login.php" method="post">
 
                     <div class="mt-2">
                         <lable>Email</lable>
-                        <input class="form-control" type="email" name="user_email">
+                        <input class="form-control" type="email" name="user_email" value="<?php if(isset($_POST['login'])){ echo $user_email; } ?>">
+
+                        <?php if(isset($_POST['login'])){ echo "<span class='text-danger'>" . $empty_email . "</span>";} ?>
                     </div>
 
                     <div class="mt-2">
                         <lable>Password</lable>
                         <input class="form-control" type="password" name="user_password">
+
+                        <?php if(isset($_POST['login'])){ echo "<span class='text-danger'>" . $empty_password . "</span>";} ?>
                     </div>
 
                     <div class="mt-2">
